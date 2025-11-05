62a53d3bb685ad0efe6108afe48cef8a79a7cd2f
Picaña Rosario <picaniarosario@gmail.com>
2025-11-05 18:44:35 +0000

    Conexión estable con Firestore y menú admin/público funcionando

--- src/app/(public)/menu/page.tsx
+++ src/app/(public)/menu/page.tsx
@@ -1,10 +1,7 @@
-import { CATEGORIES, MENU_ITEMS } from '@/lib/data';
-import MenuClient from './menu-client';
+// src/app/menu/page.tsx
+import MenuClient from "../menu/menu-client";
 
-export default async function MenuPage() {
-  // In a real app, you would fetch this data from Firestore
-  const categories = CATEGORIES.filter(c => c.isVisible).sort((a, b) => a.order - b.order);
-  const menuItems = MENU_ITEMS.filter(i => i.isVisible);
-
-  return <MenuClient categories={categories} menuItems={menuItems} />;
+export default function MenuPage() {
+  // MenuClient va a leer los platos desde Firestore
+  return <MenuClient />;
 }
