f04205b6286b03417f07440fac9577b902bf749f
Picaña Rosario <picaniarosario@gmail.com>
2025-11-12 17:41:48 +0000

    hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { useRestaurantSettings } from "@/hooks/use-restaurant-settings";
 
 export default function Home() {
   const heroImage = PlaceHolderImages.find(p => p.id === "hero-landing");
+  const heroImageMobile = PlaceHolderImages.find(p => p.id === "hero-landing-mobile");
   const settings  = useRestaurantSettings();
   const restaurantName = settings?.name || "Picaña";
 
@@ -20,12 +21,12 @@ export default function Home() {
           alt={heroImage.description}
           data-ai-hint={heroImage.imageHint}
           fill
-          className="object-cover -z-10 brightness-50"
+          className="object-cover z-10 brightness-50 object-[center_20%]"
         />
       )}
-      <div className="relative z-10 flex flex-col items-center space-y-6 text-white">
+      <div className="relative z-10 flex flex-col items-center space-y-6 mt-40 text-white">
         {/*<Utensils className="w-20 h-20 text-accent" />*/}
-        {settings?.logoUrl ? (
+       {/* {settings?.logoUrl ? (
             <img
               src={settings.logoUrl}
               alt="Logo"
@@ -36,13 +37,14 @@ export default function Home() {
           )}
         <h1 className="text-6xl md:text-8xl font-headline font-bold text-shadow-lg">
         {restaurantName}
-        </h1>
+        </h1> */}
+        
+        <Button asChild size="lg" className="bg-accent hover:bg-accent/90 text-accent-foreground font-bold text-lg py-8 px-10" style={{ color: "#fff7e4" }}>
+          <Link href="/menu" className="text-[#fff7e4]" >Ver Menú</Link>
+        </Button>
         <p className="text-xl md:text-2xl font-body max-w-2xl">
           Donde Carne y Cerveza se Hermanan.
         </p>
-        <Button asChild size="lg" className="bg-accent hover:bg-accent/90 text-accent-foreground font-bold text-lg py-8 px-10">
-          <Link href="/menu">Ver Menú</Link>
-        </Button>
       </div>
     </main>
   );
