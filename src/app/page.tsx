edf08168f635fdc9d0123a6d1389614ea7346ffe
Picaña Rosario <picaniarosario@gmail.com>
2025-11-22 17:17:04 +0000

    landing page logo and button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,46 +7,69 @@ export default function Home() {
   return (
     <main
       className="
-        relative 
-        min-h-screen 
-        flex 
-        flex-col
-        items-center 
-        justify-center 
-        text-center 
-        p-4
+        relative
+        min-h-screen
+        flex
+        flex-col                 /* ← YA NO CENTRA TODO */
         bg-[hsl(var(--nav-bg))]
-        space-y-10
+        text-center
       "
     >
 
-      {/* LOGO SIN POSITION ABSOLUTE */}
+      {/* LOGO PEGADO ARRIBA */}
       <img
         src="https://res.cloudinary.com/dq7nlctcw/image/upload/v1763664071/logo_picania_beige_xfzqk9.png"
         alt="Logo Picaña"
         className="
+          z-0
           w-[300px]
           md:w-[420px]
           lg:w-[540px]
           xl:w-[650px]
           h-auto
+          mx-auto                  /* centra horizontalmente */
+          mt-0                     /* sin margin arriba */
+          pt-0                     /* sin padding arriba */
         "
       />
 
-      {/* BOTÓN */}
-      <Button
-        asChild
-        size="lg"
-        className="bg-[#fff7e3] hover:bg-[#e8dbbd] text-[#1b3059] font-bold text-lg py-6 px-10"
+      {/* BLOQUE SUPERPUESTO */}
+      <div
+        className="
+          absolute
+          top-[48%]     /* ← ajustado porque el logo ahora está más arriba */
+          left-1/2
+          -translate-x-1/2
+          flex
+          flex-col
+          items-center
+          z-10
+        "
       >
-        <Link href="/menu">Ver Menú</Link>
-      </Button>
+        <Button
+          asChild
+          size="lg"
+          className="
+            bg-[#fff7e3]
+            hover:bg-[#e8dbbd]
+            text-[#1b3059]
+            font-bold
+            text-lg
+            py-6
+            px-10
+            mb-4
+          "
+        >
+          <Link href="/menu">Ver Menú</Link>
+        </Button>
 
-      {/* TEXTO */}
-      <p className="text-xl md:text-2xl font-body max-w-2xl text-white">
-        Donde Carne y Cerveza se Hermanan.
-      </p>
+        <p className="text-sm md:text-xl font-body text-white whitespace-nowrap mx-auto">
+  Donde Carne y Cerveza se Hermanan.
+</p>
 
+
+
+      </div>
     </main>
   );
 }
