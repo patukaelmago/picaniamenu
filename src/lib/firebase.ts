0f56e3cf60ad0cdcc5e769ae3a8699c5d6bbfdb8
Patricio Uskaer <patriciouskaer@gmail.com>
2025-12-07 18:16:10 +0000

    login1

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
@@ -15,3 +16,4 @@ const firebaseConfig = {
 export const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app);
