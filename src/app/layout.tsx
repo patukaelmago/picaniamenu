36b4ee8acbc8dab1cea8a6432a2f31da684ab39a
Patricio Uskaer <patriciouskaer@gmail.com>
2025-12-08 05:35:00 +0000

    ScrollToTopButton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-import type {Metadata} from 'next';
-import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import ScrollToTopButton from "@/components/ScrollToTopButton";
 
 export const metadata: Metadata = {
-  title: 'Picaña | Restaurante',
-  description: 'El menú de Picaña Restaurante, Rosario, AR.',
+  title: "Picaña | Restaurante",
+  description: "El menú de Picaña Restaurante, Rosario, AR.",
 };
 
 export default function RootLayout({
@@ -16,13 +17,27 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
       </head>
+
       <body className="font-body antialiased">
         {children}
+
+        {/* Botón flotante para volver arriba */}
+        <ScrollToTopButton />
+
+        {/* Notificaciones */}
         <Toaster />
       </body>
     </html>
   );
 }
+
