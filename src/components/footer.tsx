8d973270dcdb347aecc5cec0b907fe0b4a0a7deb
Patricio Uskaer <patriciouskaer@gmail.com>
2025-12-09 04:48:37 +0000

    visa

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -42,7 +42,7 @@ export default function Footer() {
             <img
               src="/visa.png"
               alt="Visa"
-              className="h-6 md:h-7 lg:h-10 w-auto opacity-80 grayscale-[40%] transition-all hover:opacity-100 hover:grayscale-0 hover:scale-105"
+              className="h-6 md:h-7 lg:h-12 w-auto opacity-80 grayscale-[40%] transition-all hover:opacity-100 hover:grayscale-0 hover:scale-105"
             />
             <img
               src="/mastercard.png"
