6e79dbac99363390d06df41a521f51db03cfc3d1
Patricio Uskaer <patriciouskaer@gmail.com>
2026-01-02 00:37:26 +0000

    p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,24 +11,24 @@ export default function Home() {
         flex
         flex-col
         items-center
-        bg-[hsl(var(--nav-bg))]
         text-center
-        pt-36        /* Subo todo el bloque un poco más */
-        md:pt-20       /* En desktop también sube */
+        pt-36
+        md:pt-20
+        bg-[hsl(var(--nav-bg))]
+        dark:bg-black
       "
     >
-
       {/* LOGO */}
       <img
         src="/logorecortado.png"
         alt="Logo Picaña"
         className="
           w-[260px]
-          md:w-[360px]      /* +20px extra */
-          lg:mt-10     
+          md:w-[360px]
+          lg:mt-10
           h-auto
-          mb-20             /* sube el botón */
-          opacity-95        /* fade MUY leve */
+          mb-20
+          opacity-95
           pt-24
         "
       />
@@ -47,18 +47,12 @@ export default function Home() {
           md:mt-10
           py-4
           px-8
-          mb-4               /* espacio mejorado */
-          shadow-sm          /* mejora visual sutil */
+          mb-4
+          shadow-sm
         "
-       >
+      >
         <Link href="/menu">Ver Menú</Link>
       </Button>
-
-      {/* TEXTO */}
-      <p className="text-base md:text-xl font-body text-white opacity-95">
-       
-      </p>
-
     </main>
   );
 }
