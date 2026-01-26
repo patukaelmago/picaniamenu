cd196de45d0f114d3bf58d1eb03db3f9e13bd9b4
Patricio Uskaer <patriciouskaer@gmail.com>
2026-01-26 04:31:44 +0000

    formatCurrency

--- src/app/admin/menu/page.tsx
+++ src/app/admin/menu/page.tsx
@@ -68,6 +68,8 @@ const formatCurrency = (price: number) => {
   return new Intl.NumberFormat("es-AR", {
     style: "currency",
     currency: "ARS",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
   }).format(price);
 };
 
