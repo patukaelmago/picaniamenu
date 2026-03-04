7506496f6b49d3b6c886331327ebb75ac4ee9138
Patricio Uskaer <patriciouskaer@gmail.com>
2026-03-04 14:05:43 +0000

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-
-"use client";
-
-import LoginPage from "@/app/admin/login/page";
+import LoginWithGoogle from "@/components/LoginWithGoogle";
 
 export default function Home() {
-  return <LoginPage />;
-};
+  return (
+    <main className="min-h-screen flex items-center justify-center p-6">
+      <div className="w-full max-w-md">
+        <LoginWithGoogle />
+      </div>
+    </main>
+  );
+}
