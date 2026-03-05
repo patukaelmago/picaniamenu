c886ca24f754b9818b1e4c912df7a7fbb98611ff
Patricio Uskaer <patriciouskaer@gmail.com>
2026-03-05 17:47:51 +0000

    tenant

---
+++ src/app/admin/laroti/page.tsx
@@ -0,0 +1,3 @@
+export default function AdminLarotiPage() {
+    return <div className="p-6">Admin La Roti</div>;
+  }
---
+++ src/app/admin/picana/page.tsx
@@ -0,0 +1,3 @@
+export default function AdminPicanaPage() {
+    return <div className="p-6">Admin Picaña</div>;
+  }
