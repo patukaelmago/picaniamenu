86ddfa4f911862e7fbb4c17a7b2933ff05bb43b2
Patricio Uskaer <patriciouskaer@gmail.com>
2026-06-21 19:44:13 +0000

    ff

--- src/app/(public)/menu/menu-client.tsx
+++ src/app/(public)/menu/menu-client.tsx
@@ -673,7 +673,7 @@ export default function MenuClient({ tenantId }: Props) {
 
                                 {shownDesc && (
                                   <p
-                                    className="mt-1 text-xs md:text-sm leading-snug max-w-3xl"
+                                    className="mt-1 text-[13px] md:text-[15px] leading-snug max-w-3xl"
                                     style={{ color: `hsl(${ui.descriptionText})` }}
                                   >
                                     {shownDesc}
