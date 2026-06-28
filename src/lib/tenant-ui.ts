386cae0c76096a687c1da47c56db13b8f12b652d
Patricio Uskaer <patriciouskaer@gmail.com>
2026-06-28 20:44:04 +0000

    test color

--- src/lib/tenant-ui.ts
+++ src/lib/tenant-ui.ts
@@ -95,7 +95,7 @@ picana: {
   // ===== ADMIN =====
 
   adminBackground: "0 0% 100%", // Fondo principal del admin y de las tarjetas (bg-card)
-  adminForeground: "43 100% 94%", // Texto principal del admin y de las tarjetas
+  adminForeground: "220 50% 23%", // Texto principal del admin y de las tarjetas
 
   adminSidebarBg: "220 50% 23%", // Fondo del menú lateral del admin
   adminSidebarText: "0 0% 100%", // Texto e íconos del menú lateral
