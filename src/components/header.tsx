2d02df91c0de0a10a6c476af795d0f1391a7071d
Patricio Uskaer <patukaelmago@gmail.com>
2026-07-29 15:32:56 -0300

    Header: aumenta el tamaño del logo del tenant

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -27,7 +27,7 @@ export default function Header() {
             <img
               src={settings.logoUrl}
               alt={settings.name || "Logo"}
-              className="h-10 md:h-12 w-auto object-contain"
+              className="h-14 w-auto object-contain md:h-16"
               onError={(e) => {
                 (e.target as HTMLImageElement).style.display = "none";
               }}
