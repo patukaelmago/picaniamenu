6a24671e3a16f6be68bad7aea8a834486dc2a57d
Patricio Uskaer <patriciouskaer@gmail.com>
2026-08-02 19:31:33 +0000

    feat: crear estructura base de restoreMaidoDemo

---
+++ src/lib/restoreMaidoDemo.ts
@@ -0,0 +1,8 @@
+import { db } from "./firebase";
+import { maidoDemo } from "@/demo-data/maido";
+
+export async function restoreMaidoDemo() {
+  console.log("Restaurando demo Maido...");
+
+  // Acá vamos a implementar la restauración completa
+}
