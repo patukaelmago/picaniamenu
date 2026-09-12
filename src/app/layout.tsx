32873349c30424955ed00132ba26482a39ab50ba
Patricio Uskaer <patukaelmago@gmail.com>
2026-09-12 15:02:22 -0300

    Instalar Google Analytics en Carta Online

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
@@ -121,6 +122,18 @@ export default function RootLayout({
       </head>
 
       <body className="font-body antialiased">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-G3EC9KBSXC"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G3EC9KBSXC');
+          `}
+        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: jsonLd(structuredData) }}
